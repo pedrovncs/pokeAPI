479f5909b29b374c6e233225635efb70ea909127
pedrovncs <pdrovncs@gmail.com>
2023-04-14 13:24:24 -0300

    Initial commit.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const qS = _ => document.querySelector(_);
 qS('#search').addEventListener('click', getPoke);
 
+function upFL(str){
+    return str.charAt(0).toUpperCase()+str.slice(1);
+}
 
 function getPoke(){
 const name = qS('#pokeName').value.toLowerCase();
@@ -8,15 +11,19 @@ const name = qS('#pokeName').value.toLowerCase();
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
     .then((data)=> {
-        qS('#pokeBox').innerHTML = 
-       '<div>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/131.png"
-                alt="Sprite Imagem">
+        qS('.pokeBox').innerHTML = 
+       `<div>
+            <img src="${data.sprites.other["official-artwork"].front_default}"
+            alt="${data.name}">
         </div>
         <div class="pokeInfo">
-            <h1>Lapras</h1>
+            <h1>${upFL(data.name)}</h1>
             <p>Descrição</p>
-        </div>'
+        </div>`
     })
-    .catch((error) => { console.log('nao encontrado' + '' + error)});
+    .catch((error) => { 
+        qS(".pokeBox").innerHTML = '<h2>Pokemon não encontrado 😭<h2>';
+        console.log('nao encontrado' + ' ' + error)
+    
+    });
 }
