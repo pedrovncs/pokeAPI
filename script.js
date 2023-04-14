0306b43ce3c24393ea0310a4c814182f4b045cd8
pedrovncs <pdrovncs@gmail.com>
2023-04-14 18:35:40 -0300

    Alterado Layout Inicial

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const qS = _ => document.querySelector(_);
 qS('#search').addEventListener('click', getPoke);
 
-function upFL(str){
-    return str.charAt(0).toUpperCase()+str.slice(1);
+function upFL(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function getPoke(){
-const name = qS('#pokeName').value.toLowerCase();
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then((response) => response.json())
-    .then((data)=> {
-        qS('.pokeBox').innerHTML = 
-       `<div>
-            <img src="${data.sprites.other["official-artwork"].front_default}"
-            alt="${data.name}">
+function getPoke() {
+    const name = qS('#pokeName').value.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => response.json())
+        .then((data) => {
+            qS('#pokedex').innerHTML = `
+            <div class="imageScreen">
+            <img src="${data.sprites.front_default}" alt="">
+            <span>#${data.id}<span>
+            <h4 class="nameTitle">${upFL(data.name)}</h4>
         </div>
-        <div class="pokeInfo">
-            <h1>${upFL(data.name)}</h1>
-            <p>Descrição</p>
-        </div>`
-    })
-    .catch((error) => { 
-        qS(".pokeBox").innerHTML = '<h2>Pokemon não encontrado 😭<h2>';
-        console.log('nao encontrado' + ' ' + error)
-    
-    });
+        <div class="dataScreen">
+            <p><b>Type:</b></p><span>$}</span><br>
+            <p><b>Height:</b></p><span>${data.height}</span><br>
+            <p><b>Weight:</b></p><span>${data.weight}</span>
+            <p><b>Gen:</b><span></span></p><br>
+            <p><b>Description:</b></p><br>
+        </div>
+        `
+        })
+        .catch((error) => {
+            qS(".imageScreen").innerHTML = '<h3>Pokemon não encontrado 😭<h2>';
+            console.log('nao encontrado' + ' ' + error)
+        });
 }
