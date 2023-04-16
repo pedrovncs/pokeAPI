d30e412956c9381cb6f76ce4b8fed81397a49bda
pedrovncs <pdrovncs@gmail.com>
2023-04-16 14:46:30 -0300

    texto animado e pequenas alterações no script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const qS = _ => document.querySelector(_);
 qS('#search').addEventListener('click', getPoke);
+qS('#pokeName').addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') getPoke();
+})
+
 
 function upFL(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -7,21 +11,19 @@ function upFL(str) {
 
 function getPoke() {
     const name = qS('#pokeName').value.toLowerCase();
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => response.json())
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((response) => response.json())
         .then((data) => {
-            qS('#pokedex').innerHTML = `
-            <div class="imageScreen">
+            qS('.imageScreen').innerHTML = `
             <img src="${data.sprites.front_default}" alt="">
             <span>#${data.id}<span>
-            <h4 class="nameTitle">${upFL(data.name)}</h4>
-        </div>
-        <div class="dataScreen">
+            <h4 class="nameTitle">${upFL(data.name)}</h4>`
+            qS('.dataScreen').innerHTML = `
             <p><b>Type:</b></p><span>$}</span><br>
             <p><b>Height:</b></p><span>${data.height}</span><br>
             <p><b>Weight:</b></p><span>${data.weight}</span>
             <p><b>Gen:</b><span></span></p><br>
-            <p><b>Description:</b></p><br>
-        </div>
+            <p><b>Description:</b></p><span></span><br>
         `
         })
         .catch((error) => {
