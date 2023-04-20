ede6dbdf4db8a3d149bfd5ee4fb5ff76c9d6360e
pedrovncs <pdrovncs@gmail.com>
2023-04-20 12:27:12 -0300

    adicionado icones aos tipos e movido para .imageScreen
    removido a animação de texto que causava o erro de overflow na .dataScreen

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function upFL(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+
 function getPoke() {
     var effect = new Audio('assets/sound.mp3');
     effect.play();
@@ -19,37 +20,46 @@ function getPoke() {
             const idPoke = data.id;
             const types = data.types.map(type => type.type.name);
             const imgPoke = data.sprites.front_default;
+            const imgPokeShiny = data.sprites.front_shiny;
             const namePoke = upFL(data.name);
             const weightPoke = data.weight;
             const heightPoke = data.height;
             fetch(data.species.url)
                 .then(response => response.json())
                 .then(dataSpecie => {
-                    console.log(dataSpecie)
                     const color = dataSpecie.color.name;
-                    const description = dataSpecie.flavor_text_entries[0].flavor_text;
+                    const description = dataSpecie.flavor_text_entries[1].flavor_text;
                     const genPoke = dataSpecie.generation.name.slice(11).toUpperCase();
 
                     qS('.imageScreen').innerHTML =
-                        `<img src="${imgPoke}" alt="${namePoke} sprite">
+                        `
+                        <img src="${imgPoke}" alt="${namePoke} sprite">
                         <span>#${idPoke}</span>
-                        <h3>${namePoke}</h3> `
+                        <h3>${namePoke}</h3>
+                        <div class='typeContainer'>
+                        <img src='assets/poketypes/${types[0]}.png' alt='${types[0]}'  onerror="this.style.display='none'" > 
+                        <img src='assets/poketypes/${types[1]}.png' alt='${types[1]}'  onerror="this.style.display='none'" > 
+                        </div>
+                        `
                     qS('.imageScreen').style = `background-image: linear-gradient(${color}, white);`;
 
                     qS('.dataScreen').innerHTML =
-                        `<h4>TYPE: </h4>
-                        <p>${types}</p>
+                        `
                         <h4>DESCRIPTION: </h4>
                         <p>${description}</p>
                         <h4>WEIGHT: </h4> 
                         <p>${weightPoke / 10}kg</p>
                         <h4>HEIGHT: </h4>
                         <p>${heightPoke / 10}m</p>
-                        <h4>GENERATION:${genPoke}</h4>`
+                        <h4>GENERATION:${genPoke}</h4>
+                        `
                 })
         })
         .catch((error) => {
             qS(".imageScreen").innerHTML = '<h3>Pokemon não encontrado 😭<h2>';
+            qS(".imageScreen").style = `background-color: white`
+            qS(".dataScreen").innerHTML = null
+
             console.log('nao encontrado: ' + ' ' + error)
         })
 }
